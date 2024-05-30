8e9e9d076074885d315678271348b4524f311077
SergioCE13 <sergioce.ipn@gmail.com>
2024-05-30 12:28:56 -0600

    Se agregaron los permisos para recibir solicitudes desde puertos externos

--- src/main/java/com/example/manosalaobrabackend/controller/VendedorController.java
+++ src/main/java/com/example/manosalaobrabackend/controller/VendedorController.java
@@ -3,8 +3,10 @@ package com.example.manosalaobrabackend.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.manosalaobrabackend.model.Vendedor;
@@ -12,6 +14,7 @@ import com.example.manosalaobrabackend.service.VendedorService;
 
 @RestController
 @RequestMapping("/api/manosalaobrabackend/vendedor")
+@CrossOrigin(origins = "*", methods = {RequestMethod.GET, RequestMethod.POST})
 public class VendedorController {
 	
 	public VendedorService vendedorService;
